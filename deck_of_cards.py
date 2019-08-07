2115f99fcf4d08cfd3dbdc9f1eb138b6a0d30b94
sgwade <52643538+sgwade@users.noreply.github.com>
2019-08-07 12:08:53 -0700

    add existing files

--- deck_of_cards.py
+++ deck_of_cards.py
@@ -1,3 +1,5 @@
+import random
+
 class Card:
     def __init__(self, suit, value):
         self.suit = suit
@@ -17,10 +19,30 @@ class Deck:
         for suit in suits:
             for value in values:
                 self.deck.append(Card(suit, value))
-    
     def shuffle(self):
-        # implement shuffle
-        pass
+        random.shuffle(self.deck)
+        return self 
+    def deal(self, *player):
+        self.shuffle()
+        for p in player:
+            for i in range(7):
+                p.hand.append(self.deck.pop())
+    def draw(self, player):
+        player.hand.append(self.deck.pop())
+
+class Player:
+    def __init__(self, hand = [], name = '', pairs = 0):
+        self.hand = hand
+        self.name = name
+        self.pairs = pairs
+    def ask(self, player, target, card):
+        for in target.hand:
+            if i.values == card:
+                player.hand.append(target.hand.pop(count))
+
+
+player1 = Player(hand = [], name = 'p1', pairs = 0)
+player2 = Player(hand = [], name = 'p2', pairs = 0)
 
 d1 = Deck()
 d1.build_deck()

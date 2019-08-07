dd588e6e3f55238b3a9cb4d52bab88eb9d0839c3
Josh <jseverett16@gmail.com>
2019-08-07 13:39:01 -0700

    Update deck_of_cards.py

--- deck_of_cards.py
+++ deck_of_cards.py
@@ -33,30 +33,52 @@ class Player:
         self.hand = hand
         self.name = name
         self.pairs = pairs
-    def ask(self, player, target, card, deck):
-        count1, count2 = 0
+
+    def display(self):
+        player_hand = '' 
+        for i in self.hand:
+            player_hand += str(i.value) + ' of ' + i.suit + ' | '
+    
+    def ask(self, target, card, deck):
+        count1 = 0
+        count2 = 0
         for i in target.hand:
             if i.value == card:
                 target.hand.pop(count1)
-                player.pairs += 1
-                for x in player.hand:
+                self.pairs += 1
+                for x in self.hand:
                     if x.value == card:
-                        player.hand.pop(count1)
+                        self.hand.pop(count1)
                         print("You got a pair!")
                         return self
                     count2 += 1
             count1 += 1
         if count2 == 0:
-            deck.draw(player)
+            deck.draw(self)
             print("Go fish!")
         return self
 
+def game_loop():
+    d1 = Deck()
+    d1.build_deck()
+    player1 = Player(hand = [], name = 'p1', pairs = 0)
+    player2 = Player(hand = [], name = 'p2', pairs = 0)
+    d1.deal(player1, player2)
+    current_player = player1
+    while len(d1) > 0:
+        if current_player == player1:
+            current_ask = input("What card are you looking for: ")
+            player1.ask (player2, current_ask, d1)
+            current_player = player2
+        else:
+            current_ask = input("What card are you looking for: ")
+            player2.ask (player1, current_ask, d1)
+            current_player = player1
+    if player1.pairs > player2.pairs:
+        print("Player 1 Wins!")
+    elif player1.pairs < player2.pairs:
+        print("Player 2 Wins!")
+    else:
+        print("It's a TIE!!!!")
 
-player1 = Player(hand = [], name = 'p1', pairs = 0)
-player2 = Player(hand = [], name = 'p2', pairs = 0)
-
-d1 = Deck()
-d1.build_deck()
-print(d1.deck)
-print(len(d1.deck))
 
